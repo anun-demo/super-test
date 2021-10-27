0733f3a0db00b3cb0af98fdb1181039e7888f592
Shai Dvash <shai.dvash@cyberark.com>
2021-10-27 11:48:53 +0300

    Updating

--- supertest/tester.py
+++ supertest/tester.py
@@ -1,10 +1,9 @@
 import time
 
-if __name__ == "__main__":
+def run():
 	print("Super test is running your tests now!")
 
 	# We don't actually run any tests, a super test is always true!
 	time.sleep(5)
 
 	print("ALL TESTS PASSED!")
-	
